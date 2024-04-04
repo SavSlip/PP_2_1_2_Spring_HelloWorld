d15ac10e28543db487fd246b6a8b8523b7db2f58
Alexandr Sukhanov <savslip@gmail.com>
2024-04-04 16:49:57 +0600

    Выполнил задание

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat catBean = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
     }
 }
